a88042701355319f756e175d58e9a2758f043177
baig <baig@localhost>
2021-09-16 20:58:24 +0530

    Maven Project

---
+++ MavenWeb2/src/main/java/com/jenk/MyCalculator.java
@@ -0,0 +1,13 @@
+package com.jenk;
+
+public class MyCalculator {
+
+		public int getsum(int x,int y)
+		{
+			return x+y;
+		}
+		public int getdiff(int x, int y)
+		{
+			return x-y;
+		}
+}
---
+++ MavenWeb2/src/test/java/MyCalculatorTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.jenk.MyCalculator;
+
+public class MyCalculatorTest {
+
+	@Test
+	public void test1() {
+		assertEquals(10,new MyCalculator().getsum(5,5));
+	}
+	@Test
+	public void test2() {
+		assertEquals(5,new MyCalculator().getsum(2,3));
+	}
+	@Test
+	public void test3() {
+		assertEquals(5,new MyCalculator().getdiff(10,5));
+	}
+	@Test
+	public void test4() {
+		assertEquals(0,new MyCalculator().getsum(5,5));
+	}
+
+
+}
